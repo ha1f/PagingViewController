3ceed3ccd10e827f2b3a26666f864c01d69bc5e9
ha1fha1f <half.ldk.hiz@gmail.com>
2015-06-30 22:37:17 +0900

    Initial Commit

---
+++ PictEdit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PictEdit
+//
+//  Created by 山口 智生 on 2015/06/30.
+//  Copyright (c) 2015年 NextVanguard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
